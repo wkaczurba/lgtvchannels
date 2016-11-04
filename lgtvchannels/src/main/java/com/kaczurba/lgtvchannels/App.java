9381178e8ab3c1d52b1c5a2cd6446bf042cc85e5
wkaczurb <wkaczurb@WKACZURB-L01.ad.analog.com>
2016-11-04 22:49:25 +0000

    Initial version

---
+++ lgtvchannels/src/main/java/com/kaczurba/lgtvchannels/App.java
@@ -0,0 +1,13 @@
+package com.kaczurba.lgtvchannels;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
